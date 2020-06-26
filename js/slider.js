07237a1a06f6f6fd93f26c411b63f2d095b5fdbe
unknown <artur.ax@gmail.com>
2020-06-26 16:41:49 +0400

    Подклыуцхил слайдер, сверстал блок Контактов.

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,16 @@
-$('.slider').slick({
-    autoplay: false,
-    autoplaySpeed: 1500,
-    dots: false,
-    prevArrow: $('#arrow_prev'),
-    nextArrow: $('#arrow_next'),
+let slider = $('.slider');
+let arrowPrev = $('#slider_prev')
+let arrowNext = $('#slider_next')
+
+
+slider.slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
     speed: 300,
-    variableWidth: true,
-});
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: arrowPrev,
+    nextArrow: arrowNext,
+    });
+    
