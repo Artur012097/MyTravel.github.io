a0f599384b31408a292bae65e9480d44c354ba96
unknown <artur.ax@gmail.com>
2020-06-26 14:26:14 +0400

    Промежуток, сверстано несколько блоков

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ for(let i = 0; i < rows.length; i++) {
     rows[i].classList.add('trending-content-row_opacity-0');
     
 }
+
+
+// Фон для блоков Трендинга 
+
+
+let tBlocks = document.querySelectorAll('.trending-content-block-top')
+
+for(let i = 0; i < tBlocks.length; i++) {
+    if(i == 8) break;
+    tBlocks[i].style.background = 'url(' +'../img/trending/' + [i + 1] + '.jpg' + ')';
+    tBlocks[i].style.backgroundSize = 'cover';
+}
---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$('.slider').slick({
+    autoplay: false,
+    autoplaySpeed: 1500,
+    dots: false,
+    prevArrow: $('#arrow_prev'),
+    nextArrow: $('#arrow_next'),
+    speed: 300,
+    variableWidth: true,
+});
