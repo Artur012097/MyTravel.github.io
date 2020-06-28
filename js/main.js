a48f216e3010c34920bdf50a3ac5b0c69894c16d
unknown <artur.ax@gmail.com>
2020-06-28 15:28:07 +0400

    Сделан адаптив.

--- js/main.js
+++ js/main.js
@@ -17,3 +17,53 @@ for(let i = 0; i < tBlocks.length; i++) {
     tBlocks[i].style.background = 'url(' +'../img/trending/' + [i + 1] + '.jpg' + ')';
     tBlocks[i].style.backgroundSize = 'cover';
 }
+
+// Скрипт для Бургер меню
+
+
+
+// Добавление класса на кнопку Бургер
+
+    burgerButton.onclick = function() {
+        let burgerButton = document.querySelector('#burgerButton');
+        let burgerOpen = 'burger-menu_open';
+        let menuNavbar = document.querySelector('#menuNavbar');
+        let menuNavbarOpen = 'header-menu_open'; 
+        let body = document.querySelector('body');
+        let bodyScrollOff = 'scroll-stop';
+
+
+        if (burgerButton.classList.contains(burgerOpen)) {
+            burgerButton.classList.remove(burgerOpen);
+        }
+        else {
+            burgerButton.classList.add(burgerOpen);
+        };
+
+            // Добавляем класс на Меню
+
+            if (menuNavbar.classList.contains(menuNavbarOpen)) {
+                menuNavbar.classList.remove(menuNavbarOpen);
+            }
+            else {
+                menuNavbar.classList.add(menuNavbarOpen);
+            };
+
+
+            if (body.classList.contains(bodyScrollOff)) {
+                body.classList.remove(bodyScrollOff);
+            }
+            else {
+                body.classList.add(bodyScrollOff);
+            };
+
+    };
+    
+    // let slickDots = document.querySelectorAll('.slick-dots li')
+    
+    // for(let i = 0; i < slickDots.length; i++) {
+    //     slickDots.classList.add('lisk');
+    //    }
+    
+
+    
--- js/slider.js
+++ js/slider.js
@@ -2,7 +2,6 @@ let slider = $('.slider');
 let arrowPrev = $('#slider_prev')
 let arrowNext = $('#slider_next')
 
-
 slider.slick({
     autoplay: true,
     autoplaySpeed: 3000,
@@ -12,5 +11,48 @@ slider.slick({
     slidesToScroll: 1,
     prevArrow: arrowPrev,
     nextArrow: arrowNext,
-    });
+    responsive: [
+      {
+        breakpoint: 998,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: true,
+        }
+      },
+        {
+          breakpoint: 650,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+          }
+      }
+    ]
+  });
+  
+// slider.slick({
+//     autoplay: true,
+//     autoplaySpeed: 3000,
+//     speed: 300,
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     prevArrow: arrowPrev,
+//     nextArrow: arrowNext,
+//     });
+
+    // Изменение типа слайдера 
+
+    // let windowScrennCheck = window.matchMedia('(max-width: 992px');
+
+    // function screenTest(e) {
+    //     if (e.matches) {
+            
+    //     }
+    // }
+
+    // screenTest(windowScrennCheck);
+
+    // windowScrennCheck.addListener(screenTest);
     
